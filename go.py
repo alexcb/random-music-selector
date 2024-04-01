08c6f4eca67156eb2de78f3a74b3c77b77045d3c
Alex Couture-Beil <alex@mofo.ca>
2024-04-01 13:42:13 -0700

    update

--- go.py
+++ go.py
@@ -2,8 +2,9 @@
 import os
 import random
 import signal
+import re
 
-from distutils.dir_util import copy_tree
+import shutil
 
 ctrl_c_exit = False
 
@@ -26,12 +27,34 @@ def get_albums(rootpath):
                     foundalbums.append(albumpath)
     return foundalbums
 
-albums = get_albums('/media/nugget_share/music/alex-beet')
+def get_ignore_regexes(path):
+    ignores = []
+    with open(path) as f:
+        for l in f.readlines():
+            l = l.strip()
+            if l.startswith('#'):
+                continue
+            ignores.append(re.compile(l, flags=re.IGNORECASE))
+    return ignores
+
+def apply_ignores(albums, ignores):
+    l = []
+    for album in albums:
+        if any(x.match(album) for x in ignores):
+            print(f'ignoring {album}')
+            continue
+        l.append(album)
+    return l
+
+ignores = get_ignore_regexes('random-music-selector-ignore')
+albums = get_albums('/media/tyee/music/')
+albums = apply_ignores(albums, ignores)
 
 dst='/media/alex/TUNES'
 
 num_albums=250
 
+print(f'randomly copying {num_albums} out of a total of {len(albums)}')
 random.shuffle(albums)
 for i, album_path in enumerate(albums):
     parts = album_path.split(os.path.sep)
@@ -39,6 +62,6 @@ for i, album_path in enumerate(albums):
     artist = parts[-2]
     d=os.path.join(dst, f'{i+1:03d}-{artist}-{album}')
     print(f'{album_path} -> {d}')
-    copy_tree(album_path, d)
+    shutil.copytree(album_path, d)
     if ctrl_c_exit or i >= num_albums-1:
         break
